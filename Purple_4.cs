91a4dbc8bb8b16d0dcc6f65428e0db614e7cc598
Vera152022 <118746256+Vera152022@users.noreply.github.com>
2025-05-03 15:10:06 +0300

    Add files via upload

---
+++ Purple_4.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8
+{
+    public class Purple_4 : Purple
+    {
+        private string _out;
+        private (string, char)[] _code;
+        public string Output => _out;
+        public Purple_4(string input, (string, char)[] code) : base(input)
+        {
+            _code = code;
+        }
+        public override string ToString()
+        {
+            return _out;
+        }
+        public override void Review()
+        {
+            if (_code == null || Input == null) return;
+            var total = new StringBuilder();
+            for (int i = 0; i < Input.Length; i++) 
+            {
+                int flag = -1;
+                for (int j = 0; j < _code.Length; j++) 
+                {
+                    if (_code[j].Item2 == Input[i])
+                    {
+                        flag = j;
+                    }
+                }
+                if(flag != -1)
+                {
+                    total.Append(_code[flag].Item1);
+                }
+                else
+                {
+                     total.Append(Input[i]);
+                }
+            }
+            _out  = total.ToString();
+        }
+
+    }
+}
