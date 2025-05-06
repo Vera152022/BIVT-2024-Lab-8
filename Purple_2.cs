92633c3d5a95c984c8fdd6fbc9a1e7ffa15659b3
Vera152022 <118746256+Vera152022@users.noreply.github.com>
2025-05-06 22:38:56 +0300

    Update Purple_2.cs

--- Purple_2.cs
+++ Purple_2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,6 +17,7 @@ namespace Lab_8
 
         public override string ToString()
         {
+            if (_out == null || _out.Length == 0) return null;
             string outt = string.Join("\n", _out);
             return outt;
         }
@@ -24,7 +25,8 @@ namespace Lab_8
         {
             if (Input == null) return;
             string[] tot = Input.Split(' ');
-            string summ = "";
+            if (tot.Length == 0) return;
+            var summ = new StringBuilder();
             int flag = 0;
             for(int i = 0; i < tot.Length; i++) 
             {
@@ -34,23 +36,23 @@ namespace Lab_8
                    
                     if (summ.Length + tot[i].Length == 50 && flag == 0)
                     {
-                        summ += tot[i];
+                        summ.Append(tot[i]);
+                        
                         flag = 1;
                     }
                         
                     else if (summ.Length + tot[i].Length < 50)
                     {
-                        if(i != tot.Length - 1)
+                        if(i < tot.Length - 1)
                         {
                             if (50 - tot[i].Length - summ.Length >= tot[i + 1].Length + 1 && flag == 0)
                             {
-                                //Console.WriteLine(tot[i]);
-                                summ += tot[i] + " ";
+                                summ.Append(tot[i]);
+                                summ.Append(" ");
                             }
                             else if(flag == 0)
                             {
-                               //Console.WriteLine(tot[i]);
-                                summ += tot[i];
+                                summ.Append(tot[i]);
                                 flag = 1;
                             }
                             
@@ -58,7 +60,7 @@ namespace Lab_8
                         }
                         else if (i == tot.Length - 1 && flag == 0)
                         {
-                            summ += tot[i];
+                            summ.Append(tot[i]);
                             flag = 1;
                         }
                      }
@@ -66,48 +68,27 @@ namespace Lab_8
                 }
                 else
                 {
-                    //Console.WriteLine(tot[i]);
-                    summ = Probel(summ);
-                    Add(summ);
-                    //Console.WriteLine(summ);
+                    var summ_3 = Probel(summ.ToString());
+                    Add(summ_3);
+                    
 
-                    //string[] time = summ.Split(' ');
-                    //int num = 0;
-                    //foreach (string s in time) 
-                    //{
-                    //    num += s.Length;
-
-                    //}
-                    //num = 50 - num;
-                    //int prob = num / (time.Length - 1);
-                    //int prob_izbran = num % (time.Length - 1);
-                    //string new_summ = time[0];
-                    //for (int j = 0; j < prob; j++) 
-                    //{ 
-                    //    if(j < prob_izbran)
-                    //        new_summ += string.Concat(Enumerable.Repeat(" ",  prob + prob_izbran));
-                    //    else
-                    //    {
-                    //        new_summ += string.Concat(Enumerable.Repeat(" ", prob));
-                    //    }
-                    //}
-
-                    summ = "";
-                    summ += tot[i] + " ";
+                    summ = new StringBuilder();
+                    summ.Append(tot[i]);
+                    if(i < tot.Length - 1)
+                      summ.Append(" ");
                     flag = 0;
                 }
             }
-            //Console.WriteLine(summ);
-            //Console.WriteLine("kkkkkkkkkkkkk");
-            summ = Probel(summ);
-            Add(summ);
-            //Console.WriteLine(summ);
-
-
+            
+            var summ_2 = Probel(summ.ToString());
+            Add(summ_2);
+            
         }
         private string Probel(string summ)
         {
             string[] time = summ.Split(' ');
+            
+            if(time.Length == 1) return time[0];
             int num = 0;
             if (time.Length == 1) return time[0];
             foreach (string s in time)
@@ -119,7 +100,7 @@ namespace Lab_8
             int prob = num / (time.Length - 1);
             
             int prob_izbran = num % (time.Length - 1);
-            //Console.WriteLine(prob_izbran);
+            
             string new_summ = time[0];
             for (int j = 0; j < time.Length - 1; j++)
             {
@@ -130,7 +111,7 @@ namespace Lab_8
                     new_summ += string.Concat(Enumerable.Repeat(" ", prob)) + time[j + 1];
                 }
             }
-            //Console.WriteLine(new_summ);
+            
             return new_summ;
         }
         private void Add(string s)
